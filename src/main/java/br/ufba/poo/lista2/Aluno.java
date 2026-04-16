ed1c2e01088e3784322c63b1363f24e9d7eeb879
luccasguimaraes-urahara <luccasguimaraes@ufba.br>
2026-04-16 06:10:42 -0300

    fim

--- src/main/java/br/ufba/poo/lista2/Aluno.java
+++ src/main/java/br/ufba/poo/lista2/Aluno.java
@@ -1,5 +1,7 @@
 package br.ufba.poo.lista2;
 
+import java.util.Objects;
+
 /**
  * Crie a classe `Aluno`, que possui os atributos `matricula` e `nome` (ambos do
  * tipo `String`), além dos métodos e construtores relevantes de acordo com a
@@ -15,8 +17,36 @@ package br.ufba.poo.lista2;
  * número de matrícula e um nome válidos, passados nessa ordem.
  */
 public class Aluno {
+    private final String matricula;
+    private String nome;
 
     public Aluno(String matricula, String nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return Objects.equals(matricula, aluno.matricula);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(matricula);
     }
 }
 
